383fe65ba1b6f842c19872699641e06ef7770ad4
v-liuson <v-liuson@MSDNUWPSERVER1>
2016-12-27 16:18:58 +0800

    12.27 itemresource

--- AutosuggestBoxSupport/MainPage.xaml.cs
+++ AutosuggestBoxSupport/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,21 +27,67 @@ namespace AutosuggestBoxSupport
         public MainPage()
         {
             this.InitializeComponent();
+            suggestions = new ObservableCollection<SampleSuggest>();//test AutosuggestBox
         }
 
-        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+
+
+        //private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        //{
+
+        //}
+
+        //private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        //{
+        //    Debug.WriteLine("AutoSuggestBox_TextChanged");
+        //}
+
+        private void EditSuggestionButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Debug.WriteLine("EditSuggestionButton_Click");
         }
+
+        private void SearchtextAutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            Debug.WriteLine("SearchtextAutoSuggestBox_SuggestionChosen");
+        }
+
+        private void SearchtextAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            Debug.WriteLine("SearchtextAutoSuggestBox_TextChanged");
+        }
+
+        private void SearchtextAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            Debug.WriteLine("SearchtextAutoSuggestBox_QuerySubmitted");
+        }
+
+
+        ///test AutosuggestBox
+        private ObservableCollection<SampleSuggest> suggestions { get; set; }
 
         private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
         {
-
+            if (args.ChosenSuggestion != null)
+                textBlock.Text = args.ChosenSuggestion.ToString();
+            else
+                textBlock.Text = sender.Text;
         }
 
         private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
+            suggestions.Clear();
+            suggestions.Add(new SampleSuggest { Index=1,Value=sender.Text});
+            suggestions.Add(new SampleSuggest { Index = 2, Value = sender.Text });
+            sender.ItemsSource = suggestions;
+        }
 
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            SampleSuggest suggest = args.SelectedItem as SampleSuggest;
+            if (suggest == null)
+                return;
+            sender.Text = suggest.Value;
         }
     }
 }
---
+++ AutosuggestBoxSupport/SampleSuggest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutosuggestBoxSupport
+{
+    public class SampleSuggest
+    {
+        public int Index { set; get; }
+        public String Value { set; get; }
+    }
+}
